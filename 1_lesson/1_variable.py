d2d6099a3a793adb035359e38c922c8b6dd9b9c1
NataliyaU <Nolace@yandex.ru>
2020-09-25 11:51:56 +0300

    1 задача

---
+++ 1_lesson/1_variable.py
@@ -0,0 +1,12 @@
+a = 345
+b = 2.45
+c = 'stroka'
+d = [23, 'hello']
+
+print(a, b, c, d)
+
+a = int(input('Введите целое число: '))
+b = float(input('Введите число с плавающей запятой: '))
+c = input('Введите текст: ')
+
+print(a, b, c)
